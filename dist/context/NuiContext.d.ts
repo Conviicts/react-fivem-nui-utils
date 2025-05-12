08369a863f6bb4c715a031853a22171e9a06905e
jode-vri <jode-vri@student.42.fr>
2025-05-12 23:48:02 +0200

    chore: remove unused NuiContext, hooks, and utility files for cleanup

--- dist/context/NuiContext.d.ts
+++
@@ -1,8 +0,0 @@
-import { IAbortableFetch } from "../providers/NuiProvider";
-export interface NuiContext {
-    resource: string;
-    callbackTimeout: number;
-    send: (e: string, data?: unknown, resource?: string) => Promise<Response>;
-    sendAbortable: (e: string, data?: unknown, resource?: string) => IAbortableFetch;
-}
-export declare const NuiContext: import("react").Context<NuiContext>;
